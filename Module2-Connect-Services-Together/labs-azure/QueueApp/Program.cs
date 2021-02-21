c38c1c59ceb1274734703e98f2bf28459f3107a4
devniel93 <danielolanoburga@gmail.com>
2021-02-21 18:54:27 -0500

    creating a new app to use az storage queues

---
+++ Module2-Connect-Services-Together/labs-azure/QueueApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QueueApp
+{
+    class Program
+    {
+        private const string ConnectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=mystoragedevniel93;AccountKey=Q3O+5E6mTCjgIOJpuofqtShm2rfnDH3dj+PNCNO01oGMvpRaCHMD1wpyBaJiokPCxK4oYKMfxlOjznSSTjta1w==";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
